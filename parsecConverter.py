5a7bd0e15242f552259d143e00e3bf340c5da86a
mahig21 <mahigawde9321@gmail.com>
2026-07-19 21:28:45 +0530

    Added function to convert parsecs to time or distance

---
+++ parsecConverter.py
@@ -0,0 +1,26 @@
+#04-05-26
+"""
+In a distant galaxy, parsecs are used to measure both time and distance.
+Given an integer number of parsecs, return its equivalent in time or
+distance.
+
+* If the given integer is odd, it represents time. If it's even, it represents
+  distance.
+
+Use these conversion rates:
+
+Parsecs | Time/Distance
+--------|--------------
+   1    |   2 hours
+   2    | 6 light years
+
+Return the converted value as an integer.
+"""
+def convert_parsecs(parsecs):
+    if parsecs%2==0:
+        ans=(parsecs*6)/2
+    else:
+        ans=2*parsecs
+    return ans
+ans=convert_parsecs(17)
+print(ans)

5f3e8045bee4973de8d1f5ae1d45e14c55dcffaf
mahig21 <mahigawde9321@gmail.com>
2026-07-20 23:55:21 +0530

    implemented binary character search in grid

---
+++ binaryCrossword.py
@@ -0,0 +1,51 @@
+#30-04-26
+"""
+Given a character, determine if its 8-bit binary representation can be found 
+in the following grid, horizontally or vertically in either direction:
+
+0 1 0 0 0 0 0 1
+0 1 1 0 1 1 1 1
+0 1 0 0 0 1 0 0
+0 1 1 0 0 1 0 1
+0 1 0 1 0 0 1 0
+0 1 0 1 0 1 0 0
+0 1 1 0 1 0 0 0
+1 0 1 0 1 1 1 0
+
+For example, "A" has the binary representation 01000001, which 
+appears in the first row from left to right.
+"""
+crossword = [
+  [0, 1, 0, 0, 0, 0, 0, 1],
+  [0, 1, 1, 0, 1, 1, 1, 1],
+  [0, 1, 0, 0, 0, 1, 0, 0],
+  [0, 1, 1, 0, 0, 1, 0, 1],
+  [0, 1, 0, 1, 0, 0, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 0],
+  [0, 1, 1, 0, 1, 0, 0, 0],
+  [1, 0, 1, 0, 1, 1, 1, 0]
+]
+
+def is_in_crossword(char):
+    target_ascii = ord(char)
+    size = len(crossword)
+    
+    for i in range(size):
+        row = crossword[i]
+        col = [crossword[j][i] for j in range(size)]
+        sequences = [
+            row,        
+            row[::-1],  
+            col,        
+            col[::-1]   
+        ]
+        
+        for seq in sequences:
+            decimal_value = int("".join(map(str, seq)), 2)
+            if decimal_value == target_ascii:
+                return True
+                
+    return False
+
+ans = is_in_crossword("I")
+print(ans)

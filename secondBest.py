d2b2f55ad833cf73ccbf459eaa0c6c9f98af98c9
mahig21 <mahigawde9321@gmail.com>
2026-08-03 23:32:12 +0530

    implemented function to find the second most expensive laptop within budget limit

---
+++ secondBest.py
@@ -0,0 +1,17 @@
+#28-08-26
+"""
+Given an array of integers representing the price of different laptops, and an integer representing your budget, return:
+
+1. The second most expensive laptop if it is within your budget, or
+2. The most expensive laptop that is within your budget, or
+3. 0 if no laptops are within your budget.
+
+* Duplicate prices should be ignored.
+"""
+def get_laptop_cost(laptops, budget):
+    if min(laptops)>budget:
+        return 0
+    for i in range(1,len(laptops)):
+        if laptops[-2]<=budget:
+            return laptops[-2]
+print(get_laptop_cost([2099, 1599, 1899, 1499], 2200))

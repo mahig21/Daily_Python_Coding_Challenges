6cc2e9399572ca47515591fc4775ce8de65c1a52
mahig21 <mahigawde9321@gmail.com>
2026-09-01 15:43:31 +0530

    implemented Tribonacci sequence generator function

---
+++ tribonacciSequence.py
@@ -0,0 +1,30 @@
+#01-09-26
+"""
+The Tribonacci sequence is a series of numbers where each number is the sum of the
+three preceding ones. When starting with 0, 0 and 1, the first 10 numbers in the
+sequence are 0, 0, 1, 1, 2, 4, 7, 13, 24, 44.
+
+Given an array containing the first three numbers of a Tribonacci sequence, and an
+integer representing the length of the sequence, return an array containing the
+sequence of the given length.
+
+- Your function should handle sequences of any length greater than or equal to zero.
+- If the length is zero, return an empty array.
+- Note that the starting numbers are part of the sequence.
+"""
+def tribonacci_sequence(start_sequence, length):
+    a=start_sequence[0]
+    b=start_sequence[1]
+    c=start_sequence[2]
+    if length<=3:
+        return start_sequence[:length]
+    else:
+        for i in range(length-3):
+            summ=a+b+c
+            start_sequence.append(summ)
+            a=b
+            b=c
+            c=summ
+        return start_sequence
+print(tribonacci_sequence([10, 20, 30], 2))
+print(tribonacci_sequence([123, 456, 789], 8))

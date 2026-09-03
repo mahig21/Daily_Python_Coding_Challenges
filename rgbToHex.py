c6fb699f290d5a6f5377dc24262758735d9a9792
mahig21 <mahigawde9321@gmail.com>
2026-09-03 22:50:20 +0530

    implemented CSS rgb to hex color converter

---
+++ rgbToHex.py
@@ -0,0 +1,30 @@
+#02-09-26
+"""
+Given a CSS rgb(r, g, b) color string, return its hexadecimal equivalent.
+
+Here are some example outputs for a given input:
+
+Input                 | Output
+--------------------- | ---------
+"rgb(255, 255, 255)"  | "#ffffff"
+"rgb(1, 2, 3)"        | "#010203"
+
+- Make any letters lowercase.
+- Return a # followed by six characters. Don't use any shorthand values.
+"""
+def rgb_to_hex(rgb):
+    num=[]
+    num_str=""
+    hexStr="#"
+    for i in rgb:
+        if i.isdigit():
+            num_str+=i
+        else:
+            if num_str!="":
+                num.append(int(num_str))
+            num_str=""
+    for i in num:
+        hexStr += f"{i:02x}"
+    return hexStr
+print(rgb_to_hex("rgb(1, 11, 111)"))
+
